8583b2d4d9f471c1f7e9b9f2b13f85591727abbc
Rina Hafizhah U <rinahafizhah@gmail.com>
2019-01-20 23:39:57 +0700

    add Button.js and make header more beautiful

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,30 +2,20 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 import logo from "../logo.svg";
+import { ButtonContainer } from "./Button";
 
-const ButtonCart = styled.button`
-  text-transform: capitalize;
-  font-size: 1.4rem;
-  background: transparent;
-  border: 0.05rem solid var(--lightBlue);
-  color: var(--lightBlue);
-  border-radius: 0.5rem;
-  padding: 0.2rem 0.5rem;
-  cursor: ponter;
-  margin: 0.2rem 0.5rem 0.2rem 0;
-  transition: all 0.5s ease-in-out;
-  &: hover {
-    background: var(--lightBlue);
-    color: var(--mainBlue);
-  }
-  &:focus {
-    outline: none;
+const NavWrapper = styled.nav`
+  background: var(--mainBlue);
+  .nav-link {
+    color: var(--mainWhite) !important;
+    font-size: 1.3rem;
+    text-transform: capitalize;
   }
 `;
 class Navbar extends Component {
   render() {
     return (
-      <nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
+      <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
         <Link to="/">
           <img src={logo} alt="logo" className="navbar-brand" />
         </Link>
@@ -37,14 +27,14 @@ class Navbar extends Component {
           </li>
         </ul>
         <Link to="/cart" className="ml-auto">
-          <ButtonCart>
+          <ButtonContainer>
             <span className="mr-2">
               <i className="fas fa-cart-plus" />
             </span>
             my cart
-          </ButtonCart>
+          </ButtonContainer>
         </Link>
-      </nav>
+      </NavWrapper>
     );
   }
 }
