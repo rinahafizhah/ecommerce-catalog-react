ad95c9cd557a96d23f1f912774ed34e67d6c0135
Rina Hafizhah U <rinahafizhah@gmail.com>
2019-01-20 23:19:10 +0700

    add header component and styled the header

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,51 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import logo from "../logo.svg";
 
+const ButtonCart = styled.button`
+  text-transform: capitalize;
+  font-size: 1.4rem;
+  background: transparent;
+  border: 0.05rem solid var(--lightBlue);
+  color: var(--lightBlue);
+  border-radius: 0.5rem;
+  padding: 0.2rem 0.5rem;
+  cursor: ponter;
+  margin: 0.2rem 0.5rem 0.2rem 0;
+  transition: all 0.5s ease-in-out;
+  &: hover {
+    background: var(--lightBlue);
+    color: var(--mainBlue);
+  }
+  &:focus {
+    outline: none;
+  }
+`;
 class Navbar extends Component {
   render() {
-    return <h3>my navbar</h3>;
+    return (
+      <nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
+        <Link to="/">
+          <img src={logo} alt="logo" className="navbar-brand" />
+        </Link>
+        <ul className="navbar-nav align-items-center">
+          <li className="nav-item ml-5">
+            <Link to="/" className="nav-link">
+              products
+            </Link>
+          </li>
+        </ul>
+        <Link to="/cart" className="ml-auto">
+          <ButtonCart>
+            <span className="mr-2">
+              <i className="fas fa-cart-plus" />
+            </span>
+            my cart
+          </ButtonCart>
+        </Link>
+      </nav>
+    );
   }
 }
 
